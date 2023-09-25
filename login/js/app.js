15db407cca3d0ced589919a6048a5e4253c6f93f
Ro SenugChan <ox0434@finenuts.co.kr>
2023-09-26 01:38:22 +0900

    생성 및 셋팅

---
+++ login/js/app.js
@@ -0,0 +1,8 @@
+const loginInput = document.querySelector("#login-form input"); 
+const loginbutton = document.querySelector("#login-form button"); 
+
+ function btnClick(){
+    console.log("click")
+}
+
+loginbutton.addEventListener("click", btnClick); 
