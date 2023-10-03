81071839c4dccf892256f9da491102820a1624f2
Ro SenugChan <ox0434@finenuts.co.kr>
2023-10-03 16:34:05 +0900

    231003

--- login/js/app.js
+++ login/js/app.js
@@ -1,13 +1,44 @@
+const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input"); 
-const loginbutton = document.querySelector("#login-form button"); 
+const greeting = document.querySelector("#greeting");
 
- function btnClick(){
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
+// const link = document.querySelector("a");
+
+
+ function onLoginSubmit(e){
+    e.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    if(value === ""){
-        alert("please")
-    }else if(username.length){
-        alert("too long")
-    }
+    console.log(username); 
+    localStorage.setItem("USERNAME_KEY", username)
+    paintGreetings(username);
+    // greeting.innerText = "Hello " + username; 
+    // greeting.innerText = `Hello ${username}`; //string과 변수를 함께 사용 시 백틱으로 표기
+    
 }
 
-loginbutton.addEventListener("click", btnClick); 
+// s. 버튼 url 이동
+// function handleLinkClick(e){
+//     e.preventDefault();
+//     console.log(e)
+//     alert("clicked!")
+// }
+// e. 버튼 url 이동
+
+// link.addEventListener("click", handleLinkClick);
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const saveUsername = localStorage.getItem("USERNAME_KEY");
+
+if (saveUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    paintGreetings(saveUsername)
+}
